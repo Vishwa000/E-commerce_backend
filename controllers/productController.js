195ba3d855f63c9dbc861ecbc5ce5a3b61850ba2
Vishwa000 <vichuvishwa004@gmail.com>
2023-11-24 12:54:29 +0530

    changed: productController.js

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,77 +1,69 @@
-const productService = require('../services/productService');
+const Product = require('../models/productModel');
+const { v4: uuidv4 } = require('uuid');
 
-const getAllProducts = async (req, res) => {
+// Controller function for creating a new product
+exports.createProduct = async (req, res) => {
   try {
-    const products = await productService.getAllProducts();
-    res.json(products);
+    // Generate a new UUID for the 'id' field
+    const id = uuidv4();
+
+    // Create a new product with the generated UUID
+    const product = await Product.create({ id, ...req.body });
+
+    res.status(201).send({ status: true, message: 'Product created successfully', product });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: 'Internal Server Error' });
+    res.status(400).send({ status: false, message: 'Failed to create product' });
   }
 };
 
-const getProductById = async (req, res) => {
-  const { id } = req.params;
+// Controller function for reading all products
+exports.getAllProducts = async (req, res) => {
+  try {
+    const products = await Product.findAll();
+    res.status(200).send({ status: true, products });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ status: false, error: 'Internal Server Error' });
+  }
+};
+
+// Controller function for updating a product by ID
+exports.updateProduct = async (req, res) => {
+  const productId = req.params.id;
 
   try {
-    const product = await productService.getProductById(id);
+    const product = await Product.findByPk(productId);
+
     if (!product) {
-      return res.status(404).json({ error: 'Product not found' });  
+      return res.status(404).send({ status: false, error: 'Product not found' });
     }
-    res.json(product);
+
+    await product.update(req.body);
+
+    res.status(200).send({ status: true, message: 'Product updated successfully', product });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: 'Internal Server Error' });
+    res.status(400).send({ status: false, error: 'Failed to update product' });
   }
 };
 
-const createProduct = async (req, res) => {
-  const productData = req.body;
+// Controller function for deleting a product by ID
+exports.deleteProduct = async (req, res) => {
+  const productId = req.params.id;
 
   try {
-    const createdProduct = await productService.createProduct(productData);
-    res.status(201).json(createdProduct);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-};
+    const product = await Product.findByPk(productId);
 
-const updateProduct = async (req, res) => {
-  const { id } = req.params;
-  const updatedProductData = req.body;
-
-  try {
-    const updatedProduct = await productService.updateProduct(id, updatedProductData);
-    if (!updatedProduct) {
-      return res.status(404).json({ error: 'Product not found' });
+    if (!product) {
+      return res.status(404).send({ status: false, error: 'Product not found' });
     }
-    res.json(updatedProduct);
+
+    await product.destroy();
+
+    res.status(200).send({ status: true, message: 'Product deleted successfully' });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: 'Internal Server Error' });
+    res.status(500).send({ status: false, error: 'Internal Server Error' });
   }
-};
-
-const deleteProduct = async (req, res) => {
-  const { id } = req.params;
-
-  try {
-    const deletedProduct = await productService.deleteProduct(id);
-    if (!deletedProduct) {
-      return res.status(404).json({ error: 'Product not found' });
-    }
-    res.json({ message: 'Product deleted successfully' });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-};
-
-module.exports = {
-  getAllProducts,
-  getProductById,
-  createProduct,
-  updateProduct,
-  deleteProduct,
 };
