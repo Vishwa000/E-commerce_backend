1448a57c388af9a54b2e7acd1de92f967763e2de
Vishwa000 <vichuvishwa004@gmail.com>
2023-11-24 13:04:41 +0530

    changed: app.js

--- app.js
+++ app.js
@@ -2,19 +2,22 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const productRoutes = require('./routes/productRoutes');
 const sequelize = require('./config/database');
-const Product = require('./models/productModel');
 
 const app = express();
-
-app.use(bodyParser.json());
-app.use('/api', productRoutes);
-
 const PORT = process.env.PORT || 8000;
 
-sequelize.sync().then(() => {
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+app.use(bodyParser.json());
+
+app.use('/api', productRoutes);
+
+sequelize
+  .sync({ force: false })
+  .then(() => {
+    console.log('Connection to the database has been established successfully.');
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error('Unable to connect to the database:', error);
   });
-}).catch((error) => {
-  console.error('Unable to connect to the database:', error);
-});
