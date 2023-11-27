a2f5dce42cc8830c58376d2eb7df036c18ef0c7f
Vishwa000 <vichuvishwa004@gmail.com>
2023-11-27 19:24:19 +0530

    add cors

--- app.js
+++ app.js
@@ -1,14 +1,32 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors')
 const productRoutes = require('./routes/productRoutes');
 const sequelize = require('./config/database');
 
 const app = express();
 const PORT = process.env.PORT || 8000;
 
+// Use the cors middleware
+app.use(cors());
+
 app.use(bodyParser.json());
 
+// JSON parsing error handling middleware
+app.use((err, req, res, next) => {
+  if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {
+    return res.status(400).send({ status: false, error: 'Invalid JSON' });
+  }
+  next();
+});
+
 app.use('/api', productRoutes);
+
+// Global error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).send({ status: false, error: 'Internal Server Error' });
+});
 
 sequelize
   .sync({ force: false })
