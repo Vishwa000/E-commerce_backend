dcaa54e535cc81c557e48314947cf3c853f3a9a0
Vishwa000 <vichuvishwa004@gmail.com>
2023-11-28 11:32:18 +0530

    route change2

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -2,9 +2,8 @@ const express = require('express');
 const router = express.Router();
 const productController = require('../controllers/productController');
 
-router.post('/products/:id', productController.createProduct);
-router.get('/products/', productController.getAllProducts);
-
+router.post('/products', productController.createProduct);
+router.get('/products', productController.getAllProducts);
 router.patch('/products/:id', productController.updateProduct);
 router.delete('/products/:id', productController.deleteProduct);
 
